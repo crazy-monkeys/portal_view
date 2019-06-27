31573d2459113f73f914ad99efdafa0299fa001c
sk <1036596437@qq.com>
2019-06-28 07:49:12 +0800

    241

--- src/components/marketing/Theme.vue
+++ src/components/marketing/Theme.vue
@@ -76,7 +76,7 @@
             </el-table-column>
             <el-table-column show-overflow-tooltip prop="" label="操作" fixed='right'>
               <template scope-slot='scope'>
-                <el-button type='text' size='small' @click='add'>明细</el-button>
+                <el-button type='primary' size='mini' @click='add'>明细</el-button>
               </template>
             </el-table-column>
             <div slot="empty">
@@ -106,239 +106,238 @@
 </template>
 
 <script>
-  import formTest from '../../assets/js/formTest'
-  export default {
-    name: 'SellIndex',
+import formTest from "../../assets/js/formTest";
+export default {
+  name: "SellIndex",
 
-    data() {
-      return {
-        options: [{
-          value: '选项1',
-          label: 'Mass Market'
-        }, {
-          value: '选项2',
-          label: 'Account Market'
-        }],
-        value: '',
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "Mass Market"
+        },
+        {
+          value: "选项2",
+          label: "Account Market"
+        }
+      ],
+      value: "",
 
-        options1: [{
-          value: '选项1',
-          label: '审批通过'
-        }, {
-          value: '选项2',
-          label: '审批驳回'
-        }, {
-          value: '选项2',
-          label: '审批中'
-        }, {
-          value: '选项2',
-          label: '草稿'
-        }],
-        value1: '',
+      options1: [
+        {
+          value: "选项1",
+          label: "审批通过"
+        },
+        {
+          value: "选项2",
+          label: "审批驳回"
+        },
+        {
+          value: "选项2",
+          label: "审批中"
+        },
+        {
+          value: "选项2",
+          label: "草稿"
+        }
+      ],
+      value1: "",
 
-        checkAll: false,
-        checkedCities: [
-          1, 2
-        ],
-        conditions: [
-          {
-            label: '客户名称',
-            value: 1
-          },
-          {
-            label: '英文名称',
-            value: 2
-          },
-          {
-            label: '客户号',
-            value: 3
-          },
-          {
-            label: '代理商',
-            value: 4
-          },
-          {
-            label: '客户类别',
-            value: 5
-          },
-          {
-            label: '报备日期',
-            value: 6
-          }
-        ],
-        isIndeterminate: false,
-        dialogVisible: false,
-        tableData: [
-          {
-            1: '审批通过',
-            2: 'Test CustomerB',
-            3: '001',
-            4: '代理商A',
-            5: 'Mass Market'
-
-          },
-          {
-            1: '审批中',
-            2: 'Test CustomerB',
-            3: '002',
-            4: '',
-            5: 'Account Market'
-
-          }
-        ],
-        //第几页
-        currentPage: 1,
-        //每页的容量
-        pageSize: 10,
-      }
+      checkAll: false,
+      checkedCities: [1, 2],
+      conditions: [
+        {
+          label: "客户名称",
+          value: 1
+        },
+        {
+          label: "英文名称",
+          value: 2
+        },
+        {
+          label: "客户号",
+          value: 3
+        },
+        {
+          label: "代理商",
+          value: 4
+        },
+        {
+          label: "客户类别",
+          value: 5
+        },
+        {
+          label: "报备日期",
+          value: 6
+        }
+      ],
+      isIndeterminate: false,
+      dialogVisible: false,
+      tableData: [
+        {
+          1: "审批通过",
+          2: "Test CustomerB",
+          3: "001",
+          4: "代理商A",
+          5: "Mass Market"
+        },
+        {
+          1: "审批中",
+          2: "Test CustomerB",
+          3: "002",
+          4: "",
+          5: "Account Market"
+        }
+      ],
+      //第几页
+      currentPage: 1,
+      //每页的容量
+      pageSize: 10
+    };
+  },
+  computed: {
+    shopId() {
+      return this.$store.state.shopId.shopId;
+    }
+  },
+  created() {},
+  watch: {},
+  methods: {
+    change() {
+      this.dialogVisible = !this.dialogVisible;
     },
-    computed: {
-      shopId() {
-        return this.$store.state.shopId.shopId
-      }
+    handleCheckAllChange(val) {
+      console.log(val);
+      this.checkedCities = val ? [1, 2, 3, 4, 5, 6] : [];
+      this.isIndeterminate = false;
     },
-    created() {
+    handleCheckedCitiesChange(value) {
+      console.log(value);
+      let checkedCount = value.length;
+      this.checkAll = checkedCount === this.conditions.length;
+      this.isIndeterminate =
+        checkedCount > 0 && checkedCount < this.conditions.length;
     },
-    watch: {
+    sure() {
+      this.dialogVisible = false;
     },
-    methods: {
-      change() {
-        this.dialogVisible = !this.dialogVisible
-      },
-      handleCheckAllChange(val) {
-        console.log(val)
-        this.checkedCities = val ? [1, 2, 3, 4, 5, 6] : [];
-        this.isIndeterminate = false;
-      },
-      handleCheckedCitiesChange(value) {
-        console.log(value)
-        let checkedCount = value.length;
-        this.checkAll = checkedCount === this.conditions.length;
-        this.isIndeterminate = checkedCount > 0 && checkedCount < this.conditions.length;
-      },
-      sure() {
-        this.dialogVisible = false
-      },
-      handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => { });
-      },
-      q(index) {
-        return this.pageSize * (this.currentPage - 1) + index + 1
-      },
-      add() {
-        this.$router.push(
-          {
-            name: 'AddSell'
-          }
-        )
-      },
-      // 分页
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-        this.pageSize = val;
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
-        this.currentPage = val;
-      },
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    },
+    q(index) {
+      return this.pageSize * (this.currentPage - 1) + index + 1;
+    },
+    add() {
+      this.$router.push({
+        name: "AddSell"
+      });
+    },
+    // 分页
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.pageSize = val;
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.currentPage = val;
     }
   }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss'>
-  $sc:12;
+$sc: 12;
 
-  .sell {
-    .head {
-      h1 {
-        opacity: 0.87;
-        font-size: 18px;
-        color: #000;
-        letter-spacing: 0;
-        line-height: 42px;
-        height: 42px;
-        /* font-weight: bold; */
-        padding: 0 50px;
-      }
-
-      .el-breadcrumb {
-        line-height: 30px;
-        margin-left: 50px;
-        margin-right: 20px;
-        font-size: 14px;
-      }
+.sell {
+  .head {
+    h1 {
+      opacity: 0.87;
+      font-size: 18px;
+      color: #000;
+      letter-spacing: 0;
+      line-height: 42px;
+      height: 42px;
+      /* font-weight: bold; */
+      padding: 0 50px;
     }
 
-    .sels {
-      background: #fff;
-      padding: 10px 30px;
-      margin: 0 20px 10px 20px;
+    .el-breadcrumb {
+      line-height: 30px;
+      margin-left: 50px;
+      margin-right: 20px;
+      font-size: 14px;
+    }
+  }
 
-      .form {
+  .sels {
+    background: #fff;
+    padding: 10px 30px;
+    margin: 0 20px 10px 20px;
 
-        /* max-width: 1000px; */
-        .el-form-item__label {
-          height: 30px;
-        }
+    .form {
+      /* max-width: 1000px; */
+      .el-form-item__label {
+        height: 30px;
+      }
 
-        .el-form-item {
-          width: 200px;
-          margin-bottom: 0;
-        }
+      .el-form-item {
+        width: 200px;
+        margin-bottom: 0;
+      }
 
-        .date {
+      .date {
+        width: 414px;
+
+        .el-date-editor {
           width: 414px;
-
-          .el-date-editor {
-            width: 414px;
-          }
-        }
-
-      }
-    }
-
-    .box {
-      margin: 0 20px 20px 20px;
-      background: #FFFFFF;
-      box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.05);
-      border-radius: 2px;
-      position: relative;
-
-      .btns {
-        .add {
-          margin: 12px 0 12px 30px;
-        }
-      }
-
-      .tab {
-        .el-table {
-          td {
-            height: 64px;
-            line-height: 64px;
-
-            .cell {
-              font-size: 12px;
-              color: #333333;
-              letter-spacing: 0;
-              line-height: 18px;
-            }
-          }
-        }
-      }
-
-      .block {
-        padding: 10px;
-
-        .el-pagination {
-          width: 100%;
-          text-align: center;
         }
       }
     }
   }
+
+  .box {
+    margin: 0 20px 20px 20px;
+    background: #ffffff;
+    box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.05);
+    border-radius: 2px;
+    position: relative;
+
+    .btns {
+      .add {
+        margin: 12px 0 12px 30px;
+      }
+    }
+
+    .tab {
+      .el-table {
+        td {
+          height: 64px;
+          line-height: 64px;
+
+          .cell {
+            font-size: 12px;
+            color: #333333;
+            letter-spacing: 0;
+            line-height: 18px;
+          }
+        }
+      }
+    }
+
+    .block {
+      padding: 10px;
+
+      .el-pagination {
+        width: 100%;
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
