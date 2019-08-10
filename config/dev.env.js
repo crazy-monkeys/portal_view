3ee2629203842558f100736a2596753f96d3a639
shareluck <1036596437@qq.com>
2019-08-10 13:00:06 +0800

    本地链条换进

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,8 +6,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    // API_ROOT: '"http://192.168.101.15:8080/portal-api"'
-    API_ROOT: '"http://47.100.106.186/portal-api"'
+    API_ROOT: '"http://192.168.101.15:8080/portal-api"'
+        // API_ROOT: '"http://47.100.106.186/portal-api"'
         // API_ROOT: '"http://192.168.101.14:8080/portal-api"'
         // API_ROOT: '"http://192.168.101.16/portal-api"'
         // API_ROOT: '"http://192.168.101.10:8080/portal-api"'
