13dace6cca439d78993e0a91fc551f38887cfbf1
junguohe <wy!950202>
2019-10-27 12:04:57 +0800

    ii

--- config/test.env.js
+++ config/test.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"testing"',
-   API_ROOT: '"http://portal-sit.threeape.cn/portal-api"'
+//    API_ROOT: '"http://portal-sit.threeape.cn/portal-api"'
         //  API_ROOT: '"https://dmsdev.unisoc.com/portal-api"'
+         API_ROOT: '"https://dms.unisoc.com/portal-api"'
 }
