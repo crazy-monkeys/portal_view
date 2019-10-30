4aeed640b94a85828ec3c54fc047449fcd6f53a7
shareluck <1036596437@qq.com>
2019-10-30 17:59:48 +0800

    业务管理部

--- src/components/price/com/tem.vue
+++ src/components/price/com/tem.vue
@@ -39,9 +39,9 @@
                 width="100"
                 >
                  <template slot-scope="scope">
-                <span v-if="scope.row.bu=='Connectivity Device BU'">泛连接</span>
-                <span v-if="scope.row.bu=='Industrial Electronics BU'">工业电子</span>
-                <span v-if="scope.row.bu=='Consumer Electronics BU'">消费电子</span>
+                <span v-if="scope.row.bu=='Connectivity Device BU'">泛连接业务管理部</span>
+                <span v-if="scope.row.bu=='Industrial Electronics BU'">工业电子业务管理部</span>
+                <span v-if="scope.row.bu=='Consumer Electronics BU'">消费电子业务管理部</span>
               </template>
                 </el-table-column>
                 <el-table-column
