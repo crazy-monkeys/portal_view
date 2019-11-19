027aecf682ee145954bc7870d93ea6b93531d834
shareluck <1036596437@qq.com>
2019-11-19 17:57:52 +0800

    销售单查询分页

--- src/components/order/list.vue
+++ src/components/order/list.vue
@@ -697,10 +697,12 @@ export default {
     handleSizeChange(val) {
       // //console.log(`每页 ${val} 条`);
       this.pageSize = val;
+      this.getList()
     },
     handleCurrentChange(val) {
       // //console.log(`当前页: ${val}`);
       this.currentPage = val;
+      this.getList()
     }
   }
 };
